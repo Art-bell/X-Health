ee11d05eec159697b22e61af7783e0ffb2d6d4fc
Arthur Bello <belloayo24@gmail.com>
2021-01-25 23:25:16 -0800

    Bootstrap added to projct and dependencies installed locally with Yarn

--- src/app/home.module.ts
+++ src/app/home.module.ts
@@ -2,20 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
-export class AppModule { }
+export class HomeModule { }
